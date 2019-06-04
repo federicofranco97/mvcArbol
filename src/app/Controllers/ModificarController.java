77f27169aea1a74b63ffdac62274b6d29b6cbea4
federicofranco97 <federicoafranco97@gmail.com>
2019-06-04 18:29:24 -0300

    final changes

--- src/app/Controllers/ModificarController.java
+++ src/app/Controllers/ModificarController.java
@@ -1,6 +1,9 @@
 
 package app.Controllers;
 
+import app.DTOs.PersonaDTO;
+import app.Servicios.Operaciones;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ModificarController {
     
+    @Autowired
+    Operaciones operaciones;
+    
     @GetMapping("/modificar")
-    public ModelAndView traerVista(@RequestParam("id") String id){
+    public ModelAndView traerVista(@RequestParam("id") long id){
+        PersonaDTO personaDTO=operaciones.buscarIdDTO(id);
         ModelAndView vista = new ModelAndView("modificar");
+        vista.addObject("persona",personaDTO);
         return vista;
     }
     
