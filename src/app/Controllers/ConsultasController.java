1eeae6df5141b2ad397f76a2a1f9b910d6adac48
federicofranco97 <federicoafranco97@gmail.com>
2019-06-06 21:11:35 -0300

    added cargar lista personas

--- src/app/Controllers/ConsultasController.java
+++ src/app/Controllers/ConsultasController.java
@@ -1,5 +1,9 @@
 package app.Controllers;
 
+import app.DTOs.PersonaDTO;
+import app.Servicios.Operaciones;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ConsultasController {
     
+    @Autowired
+    Operaciones operaciones;
+    
     @GetMapping("/consultas")
     public ModelAndView traerConsultas(){
         ModelAndView vista = new ModelAndView("consultas");
-        
+        ArrayList<PersonaDTO> lista = operaciones.getLista();
+        vista.addObject("listaPersonas", lista);
         return vista;
     }
 }
