52728fafa9dbe09af40758b70290f02d55e9dc37
beelanselm <devicente.mbelen@gmail.com>
2019-06-10 20:21:27 -0300

    vista setear

--- src/app/Controllers/SetearController.java
+++ src/app/Controllers/SetearController.java
@@ -1,15 +1,21 @@
 package app.Controllers;
 
+import app.Servicios.Operaciones;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SetearController {
+    @Autowired
+    Operaciones operaciones;
+    
     
     @GetMapping("/setRelaciones")
     public ModelAndView traerSet(){
         ModelAndView vista = new ModelAndView("setear");
+        vista.addObject("listaPersonas", operaciones.getLista());
         return vista;
     }
     
